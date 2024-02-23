e6d6f2c8395ce3613f0bef7d1862076e0cfad214
mingman <ingman.m@gmail.com>
2024-02-23 10:25:25 +0100

    example from todays lecture

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 export function App() {
+  const [count, setCount] = useState(0);
+
+  const handleOnClick = () => {
+    setCount((preVal) => {
+      const newVal = preVal + 1;
+      return newVal;
+    });
+  };
+  const handleOnResetClick = () => {
+    setCount(0);
+  };
+
   return (
     <>
       <h1>This is the App component</h1>
+      <div>
+        <p>You clicked {count} times</p>
+        <button onClick={handleOnClick}>Click me</button>
+        <button onClick={handleOnResetClick}>Reset</button>
+      </div>
     </>
   );
 }
