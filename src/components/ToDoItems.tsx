ddb050b77e8756d68bcad1f4ce7cc729d93340d7
mingman <ingman.m@gmail.com>
2024-02-26 11:43:22 +0100

    adding symbols to cards

--- src/components/ToDoItems.tsx
+++ src/components/ToDoItems.tsx
@@ -7,9 +7,17 @@ export function ToDoItems(): JSX.Element {
         {ToDos.map(function (toDo) {
           return (
             <article className="toDoCard" key={toDo.index}>
-              <h2>{toDo.title}</h2>
+              <h2>
+                {" "}
+                <span className="material-symbols-outlined">check</span>
+                {toDo.title}
+              </h2>
               <p>Written by: {toDo.author}</p>
               <p>{toDo.desc}</p>
+
+              <span className="material-symbols-outlined">inventory_2</span>
+              <span className="material-symbols-outlined">arrow_downward</span>
+              <span className="material-symbols-outlined">arrow_upward</span>
             </article>
           );
         })}
