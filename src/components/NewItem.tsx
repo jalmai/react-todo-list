ac8e812e2727c3c0e953862d178b0bd9ab0983e8
mingman <ingman.m@gmail.com>
2024-02-26 08:09:10 +0100

    Form updates todo item

--- src/components/NewItem.tsx
+++ src/components/NewItem.tsx
@@ -1,10 +1,38 @@
+import { useState } from "react";
 export function NewItem() {
+  const [form, setForm] = useState({
+    title: "",
+    desc: "",
+    author: "",
+  });
+  const handleChange = (e: any) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+    console.log(form);
+  };
   return (
     <>
       <form className="newItem">
-        <input type="text" name="title" placeholder="To do"></input>
-        <input type="text" name="desc" placeholder="Tell me more"></input>
-        <input type="text" name="author" placeholder="Author"></input>
+        <input
+          type="text"
+          onChange={handleChange}
+          name="title"
+          placeholder="To do"
+        ></input>
+        <input
+          type="text"
+          onChange={handleChange}
+          name="desc"
+          placeholder="Tell me more"
+        ></input>
+        <input
+          type="text"
+          onChange={handleChange}
+          name="author"
+          placeholder="Author"
+        ></input>
         <input type="submit" name="submit"></input>
       </form>
     </>
