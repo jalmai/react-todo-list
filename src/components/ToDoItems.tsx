c05924f337a36fb626318593058fc34080af3a90
mingman <ingman.m@gmail.com>
2024-02-26 11:30:08 +0100

    styling todo cards

--- src/components/ToDoItems.tsx
+++ src/components/ToDoItems.tsx
@@ -3,15 +3,17 @@ import { ToDos } from "../assets/items.json";
 export function ToDoItems(): JSX.Element {
   return (
     <>
-      {ToDos.map(function (toDo) {
-        return (
-          <article className="toDoCard" key={toDo.index}>
-            <h2>{toDo.title}</h2>
-            <p>{toDo.desc}</p>
-            <p>Written by: {toDo.author}</p>
-          </article>
-        );
-      })}
+      <section className="toDoItems">
+        {ToDos.map(function (toDo) {
+          return (
+            <article className="toDoCard" key={toDo.index}>
+              <h2>{toDo.title}</h2>
+              <p>Written by: {toDo.author}</p>
+              <p>{toDo.desc}</p>
+            </article>
+          );
+        })}
+      </section>
     </>
   );
 }
